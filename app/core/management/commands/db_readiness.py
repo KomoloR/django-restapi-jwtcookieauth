efa91a53811caeb34d91b91cc0268f1da9250974
KomoloR <robertjkomolo@gmail.com>
2025-10-01 10:52:48 +0300

    postgres DB setup

---
+++ app/core/management/commands/db_readiness.py
@@ -0,0 +1,24 @@
+# django command to ensure the PostgreSQL database is ready 
+
+import time
+from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
+
+from psycopg import OperationalError as PsycopgOpError
+
+
+class Command(BaseCommand):
+    # django command waiting for db readiness 
+    def handle(self, *args, **options):
+        # entry point for command 
+        self.stdout.write("Waiting for database readiness....")
+        is_db_up=False
+        while is_db_up is False:
+            try:
+                self.check(databases=["default"])
+                is_db_up=True
+            except (PsycopgOpError, OperationalError):
+                self.stdout.write("Database not ready yet....")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database now ready"))
